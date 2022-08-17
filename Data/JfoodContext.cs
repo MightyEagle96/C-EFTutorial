2d9c760c2bfd1e65df183709252840845fc04c03
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-08-17 14:55:29 +0100

    made changes

---
+++ Data/JfoodContext.cs
@@ -0,0 +1,21 @@
+﻿using EFrefresher.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFrefresher.Data
+{
+    public class JfoodContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+
+        public DbSet<Order> Orders { get; set; } = null!;
+
+        public DbSet<Product> Products { get; set; } = null!;
+
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=ME-CORP\SQLEXPRESS;Initial Catalog=JTables;Integrated Security=True");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using EFrefresher.Data;
+using EFrefresher.Models;
+
+using JfoodContext context = new JfoodContext();
+
+//to write to the database
+//Product product = new Product() { Name = "Vanilla Ice Cream", Price = 10.10M };
+//context.Products.Add(product);
+
+
+ //Product product2 = new Product() { Name = "GOlden Morn", Price = 45.44M };
+ //context.Add(product2);
+ 
+ //context.SaveChanges();
+
+//to read from the database
+//var products = context.Products.Where(p=>p.Price >10).OrderBy(p=>p.Name);
+
+//foreach (Product p in products)
+//{
+//    Console.WriteLine($"Id: {p.Id} | Name: {p.Name} | Price: {p.Price} ");
+//    Console.WriteLine(new String('-', 20));
+//}
+
+////to update the record
+//var okpa = context.Products.Where(p => p.Name == "Okpa").FirstOrDefault();
+
+//if(okpa is Product)
+//{
+//    okpa.Name = "Okpa Di Okwu";
+//}
+//context.SaveChanges();
+
+//Console.WriteLine("Product updated successfully");
+
+//to delete a record
+var product = context.Products.Where(c=>c.Id==2).FirstOrDefault();
+
+if(product is not null)
+{
+    context.Remove(product);
+    Console.WriteLine($"{product.Name} has been removed");
+}
+context.SaveChanges();
+
